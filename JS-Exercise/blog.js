3ade4144a1521e540d16316b6c37f458c7186e9f
pthesing <pthesing@gmail.com>
2016-07-07 18:41:38 -0700

    move to proper file

---
+++ JS-Exercise/blog.js
@@ -0,0 +1,25 @@
+var link = document.querySelector(".like-link");
+var likeCount = document.querySelector(".like-count");
+var commentForm = document.querySelector("#new-comment");
+var commentBody = document.querySelector("#new-comment-body")
+var comments = document.querySelector("#comments");
+
+link.addEventListener("click", like);
+commentForm.addEventListener("submit", createComment);
+
+function like(event) {
+  event.preventDefault();
+
+  var count = parseInt(likeCount.textContent);
+  likeCount.textContent = count + 1;
+  };
+
+function createComment(event) {
+  event.preventDefault();
+
+  var comment = document.createElement("div");
+  comment.setAttribute("class", "comment");
+  comment.textContent = commentBody.value;
+  comments.appendChild(comment);
+  commentForm.reset();
+};
