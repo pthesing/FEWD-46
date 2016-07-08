8bcded2ffe95548405864535c79ed9b9a41f4135
pthesing <pthesing@gmail.com>
2016-07-07 21:16:56 -0700

    fixing

--- acme-corp-built-with-js/js/setup-page.js
+++ acme-corp-built-with-js/js/setup-page.js
@@ -8,6 +8,42 @@ a.setAttribute("href", "index.html");
 
 var img = document.createElement("img");
 img.setAttribute("src", "images/acme-corp.jpg");
+
 body.appendChild(header);
 header.appendChild(a);
 a.appendChild(img);
+
+var nav = document.createElement("nav");
+
+body.appendChild(header);
+header.appendChild(nav);
+
+var ol = document.createElement("ol");
+
+body.appendChild(header);
+header.appendChild(nav);
+nav.appendChild(ol);
+
+var li1 = document.createElement("li");
+
+body.appendChild(header);
+header.appendChild(nav);
+nav.appendChild(ol);
+ol.appendChild(li);
+
+var a1 = document.createElement("a");
+a1.setAttribute("href", "products.html");
+a1.textContent = "Products";
+
+body.appendChild(header);
+header.appendChild(nav);
+nav.appendChild(ol);
+ol.appendChild(li);
+li.appendChild(a);
+
+var li2 = document.createElement("li");
+
+body.appendChild(header);
+header.appendChild(nav);
+nav.appendChild(ol);
+ol.appendChild(li);
