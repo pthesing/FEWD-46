cae71c91a4b1f438900ce1a96b0f29368911c13b
pthesing <pthesing@gmail.com>
2016-07-14 15:01:09 -0700

    to-do list!

--- todo-list/script.js
+++ todo-list/script.js
@@ -1,11 +1,25 @@
 var form = document.querySelector("#new-item-form");
+var input = document.querySelector("#new-item-input");
+var list = document.querySelector("#todo-list");
+
 
 function formSubmitted(event) {
   event.preventDefault();
 
-  // Your code goes here...
+  var li = document.createElement("li");
+  var label = document.createElement("label");
+  var checkbox = document.createElement("input");
+  var span = document.createElement("span");
+
+  checkbox.setAttribute("type", "checkbox");
+  span.textContent = input.value;
+
+  label.appendChild(checkbox);
+  label.appendChild(span);
+  li.appendChild(label);
+  list.appendChild(li);
 
   form.reset();
-}
+  }
 
 form.addEventListener("submit", formSubmitted);
