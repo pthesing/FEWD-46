3913c802ec2ff7aa075ff830b4c68aeb4404cd64
pthesing <pthesing@gmail.com>
2016-08-14 19:46:23 -0700

    NO VS YES

--- Final-Project/final.js
+++ Final-Project/final.js
@@ -1,11 +1,11 @@
 //When yes is checked
   //display appropriate vote
-  //put data in table at the bottom
 
+//51
 var yes51 = document.querySelector("#yes51");
-yes51.addEventListener("click", voteYes);
+yes51.addEventListener("click", voteYes51);
 
-function voteYes () {
+function voteYes51 () {
   var voteYes51 = document.createElement("div");
   voteYes51.setAttribute("class", "voteYes");
 
@@ -16,6 +16,53 @@ function voteYes () {
   prop51.appendChild(voteYes51);
 
 }
+
+var no51 = document.querySelector("#no51");
+no51.addEventListener("click", voteNo51);
+
+function voteNo51 () {
+  var voteNo51 = document.createElement("div");
+  voteNo51.setAttribute("class", "voteNo");
+
+  var noText = document.createTextNode("You should vote NO");
+  voteNo51.appendChild(noText);
+
+  var prop51 = document.querySelector(".prop51");
+  prop51.appendChild(voteNo51);
+}
+
+//52
+
+//53
+
+//54
+
+//55
+
+//56
+
+//57
+
+//58
+
+//59
+
+//60
+
+//61
+
+//62
+
+//63
+
+//64
+
+//65
+
+//66
+
+//67
+
 
 
 //When no is checked
