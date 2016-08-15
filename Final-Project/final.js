f8ea04b802131ee12d94439d741f43e7ef7bdae2
pthesing <pthesing@gmail.com>
2016-08-14 19:08:13 -0700

    redoing checkboxes and formatting

--- Final-Project/final.js
+++ Final-Project/final.js
@@ -2,14 +2,10 @@
   //display appropriate vote
   //put data in table at the bottom
 
+
 //When no is checked
   //display appropriate vote
   //put data in table at the bottom
 
-//click to enlarge
 
-$(".back").toggle(function(){
-    $(this).animate({"height": "400px"}, {"width": "700px"}, 100);
-}, function(){
-    $(this).animate({"height": "260px"}, {"width": "300px;"}, 100);
-});
+//click to enlarge
